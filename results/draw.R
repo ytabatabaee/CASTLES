34cc23ea887fdbeb1c013c017d47abd356e7b322
Siavash Mirarab <smirarab@ucsd.edu>
2023-01-10 12:47:31 -0800

    update figures

--- results/draw.R
+++ results/draw.R
@@ -12,7 +12,7 @@ s[s$Method=="CASTLES (Taylor 33, Handle OG)","Method"]="CASTLES"
 
 names(s)=c("X"             ,      "Condition"         ,  "Method"        ,      "replicate"    ,       "Branch.Type"  ,
            "l.true"      ,        "l.est"        ,       "log10.l.true."   ,   
-           "log10.l.est."    ,    "abserr"   ,"log10err" )
+           "log10.l.est."    ,    "abserr"   ,"log10err" ,"se")
 
 ggplot(aes(x=l.true,y=l.est,color=Branch.Type,linetype=Branch.Type),
        data=s[!variants,])+
@@ -98,3 +98,18 @@ ggplot(aes(x=reorder(sub("_non","bp",sub("fasttree_genetrees_","",Condition)),se
   coord_cartesian(ylim=c(0,0.095))
 ggsave("S100-error-rmse.pdf",width=5,height = 4.7)
 
+ggplot(aes(x=reorder(sub("_non","bp",sub("fasttree_genetrees_","",Condition)),log10err),
+           y=log10err,color=sub("+","\n",Method,fixed=T)),
+       data=dcast(data=s[!variants,],Condition+Method+replicate~'log10err' ,value.var = "log10err",fun.aggregate = function(x) mean(abs(x))))+
+  scale_y_continuous(trans="identity",name="Root mean square error")+
+  geom_boxplot(outlier.alpha = 0.3,width=0.86)+
+  stat_summary(position = position_dodge(width=0.86))+
+  #geom_boxplot(outlier.size = 0)+
+  scale_fill_brewer(palette = "Dark2")+
+  scale_color_brewer(palette = "Dark2",name="")+
+  theme_bw()+
+  theme(legend.position = c(.36,.92), legend.direction = "horizontal",
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(angle=0))+
+  coord_cartesian(ylim=c(0,1.5))
+ggsave("S100-error-logabs.pdf",width=5,height = 4.7)
