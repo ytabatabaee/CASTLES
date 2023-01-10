8aff9af72a2d870e75aa0130a5095e390ab0c3d0
Siavash Mirarab <smirarab@ucsd.edu>
2023-01-09 21:16:27 -0800

    add bias figure

--- results/draw.R
+++ results/draw.R
@@ -22,3 +22,21 @@ ggplot(aes(x=l.true,y=l.est,color=Branch.Type,linetype=Branch.Type),
   theme_bw()+
   theme(legend.position = c(.24,.1))
 ggsave("S100-correlation.png",width=12.5,height = 9)
+
+
+ggplot(aes(x=sub("+","\n",Method,fixed=T),
+           y=l.true-l.est,color=Branch.Type),
+       data=s[!variants,])+
+  facet_wrap(~sub("_non","bp",sub("fasttree_genetrees_","",Condition)),ncol=1)+
+  #scale_x_continuous(trans="identity",name="True length")+
+  scale_y_continuous(trans="identity",name="True - Estimated length (bias)")+
+  stat_summary(fun.data = mean_sdl,position = position_dodge(width=0.5))+
+  #geom_boxplot(outlier.size = 0)+
+  scale_fill_brewer(palette = "Dark2")+
+  scale_color_brewer(palette = "Dark2",name="")+
+  geom_hline(color="blue",linetype=3,yintercept = 0)+
+  theme_bw()+
+  theme(legend.position = c(.23,.975), legend.direction = "horizontal",
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(angle=0))
+ggsave("S100-bias.pdf",width=5,height = 9.2)
