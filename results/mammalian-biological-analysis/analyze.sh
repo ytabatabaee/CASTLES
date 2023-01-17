8a69548b5ecf8ad3f267fdb517bfdbee0e569355
Siavash Mirarab <smirarab@ucsd.edu>
2023-01-17 09:53:06 -0800

    update script

--- results/mammalian-biological-analysis/analyze.sh
+++ results/mammalian-biological-analysis/analyze.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+nw_prune ../logs/genetrees.tre GAL > genetees-nogal.tre
 python3 ~/Research/projects/local/ASTRAL-u/ASTRAL-u/castles.py -t <(  ~/workspace/ASTER/bin/astral -C -i genetees-nogal.tre -c <( nw_reroot astralv5.7.8.tre GAL ) -o aster-noGALgenetrees.tre --root GAL|nw_prune - GAL  ) -g genetees-nogal.tre  -o castle-test4.tre
